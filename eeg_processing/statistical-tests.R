6f5873774b4ce7f2cd425de61a4ad5a1134c529c
Andreas Knoben <mail@andreasknoben.nl>
2022-03-05 16:34:48 +0100

    Generate plots

--- eeg_processing/statistical-tests.R
+++ eeg_processing/statistical-tests.R
@@ -1,3 +1,6 @@
+# Load libraries
+library(ggplot2)
+
 # Set working directory to /eeg_processing/ folder before starting!
 setwd("~/Nextcloud/Projects/cognitive-workload/eeg_processing/")
 
@@ -29,7 +32,8 @@ check_assumptions <- function(control, treatment) {
 }
 
 statistical_test <- function(control, treatment, norm_viol, output) {
-  cat("Statistics generated for the problem-solving questions", file = output, append = FALSE, sep = "\n")
+  output_file = paste(output, "stat_result.txt",sep="")
+  cat("Statistics generated for the problem-solving questions", file = output_file, append = FALSE, sep = "\n")
   
   for (iChan in 1:NCHANS) {
     concat_data <- c(control[iChan], treatment[iChan])
@@ -41,9 +45,46 @@ statistical_test <- function(control, treatment, norm_viol, output) {
     } else {
       test = t.test(indices ~ condition, data = test_data, var.equal = FALSE, na.rm = TRUE)
     }
-    cat(CHANS[iChan], file = output, append = TRUE, sep = "")
-    cat(": \t", file = output, append = TRUE, sep = "")
-    cat(toString(test), file = output, append = TRUE, sep = "\n")
+    cat(CHANS[iChan], file = output_file, append = TRUE, sep = "")
+    cat(": \t", file = output_file, append = TRUE, sep = "")
+    cat(toString(test), file = output_file, append = TRUE, sep = "\n")
+  }
+}
+
+plot_data <- function(control, treatment, output) {
+  layout <- "
+  ######OO####PP######
+  ######OO####PP######
+  #####LL##MM##NN#####
+  #####LL##MM##NN#####
+  #AA##BB##CC##DD##EE#
+  #AA##BB##CC##DD##EE#
+  #####FF##GG##HH#####
+  #####FF##GG##HH#####
+  ####II###JJ###KK####
+  ####II###JJ###KK####
+  "
+  
+  plots <- vector('list', NCHANS)
+  
+  for (iChan in 1:NCHANS) {
+    i <- iChan
+    concat_data <- c(control[iChan], treatment[iChan])
+    plot_data <- data.frame(condition = rep(c("control", "treatment"), each = NPARTS/2),
+                            indices = unlist(concat_data))
+    plot <- ggplot(data = plot_data) +
+      geom_boxplot(aes(x = condition, y = indices),
+                   fill = c("#56B4E9", "#009E73")) +
+      labs(title = CHANS[iChan]) +
+      theme(axis.title.x = element_blank(),
+            axis.title.y = element_blank(),
+            axis.text.x = element_blank(),
+            axis.ticks.x = element_blank(),
+            axis.text.y = element_text(size = 20),
+            plot.title = element_text(size = 28, hjust = 0.5))
+    svg(paste(output, "chan", CHANS[iChan], ".svg", sep=""), width = 3.25, height = 3.75)
+    print(plot)
+    dev.off()
   }
 }
 
@@ -51,8 +92,9 @@ statistical_test <- function(control, treatment, norm_viol, output) {
 open_control_FE <- read.csv("results/open/indices-control-FE.csv")
 open_treatment_FE <- read.csv("results/open/indices-treatment-FE.csv")
 
-#Output file
-output_file = "results/open/stat_result.txt"
+#Output folder
+output_dir = "results/open/"
 
 norm_violated = check_assumptions(open_control_FE, open_treatment_FE)
-statistical_test(open_control_FE, open_treatment_FE, norm_violated, output_file)
+statistical_test(open_control_FE, open_treatment_FE, norm_violated, output_dir)
+plot_data(open_control_FE, open_treatment_FE, output_dir)
