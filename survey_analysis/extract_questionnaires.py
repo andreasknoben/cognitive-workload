a24233cd883bfa7cffdbb5acc416a85f3c1f5b42
Andreas Knoben <mail@andreasknoben.nl>
2022-04-13 14:46:01 +0200

    Replaced quotation marks to match syntax

--- survey_analysis/extract_questionnaires.py
+++ survey_analysis/extract_questionnaires.py
@@ -3,7 +3,7 @@ import numpy as np
 from load_data import import_survey
 
 N_SUBJ = 58
-DATA_LOC = "survey_data.csv"
+DATA_LOC = 'survey_data.csv'
 pd.options.display.max_colwidth = 10000
 
 def generate_cols(input_df, type, *cols):
@@ -48,17 +48,17 @@ def write_output(data):
     '''
     output = pd.DataFrame()
 
-    output = save_cols(output, generate_cols(data, int, "Q113"), "age")
-    output = save_cols(output, generate_cols(data, str, "Q114"), "gender")
-    output = save_cols(output, generate_cols(data, str, "ERK", "DBK"), "erd.exp", "db.exp")
-    output = save_cols(output, generate_cols(data, str, "BTK", "BTK_1", "BTK_2", "BTK_3", "BTK_4", "BTK_5"), 
-                                                        "BTK.1", "BTK.2", "BTK.3", "BTK.4", "BTK.5", "BTK.6")
-    output = save_cols(output, generate_cols(data, str, "RFK", "RPK_1", "RPK_2", "RPK_3", "RPK_4", "RPK_5"),
-                                                        "RFK.1", "RFK.2", "RFK.3", "RFK.4", "RFK.5", "RFK.6")
-    output = save_cols(output, generate_cols(data, str, "Understand1", "Understand2", "Use1", "Use2R", "Load1", "ENG1", "ENG2", "ENG3"),
-                        "understand.1", "understand.2", "use.1", "use.2", "load", "eng.1", "eng.2", "eng.3")
+    output = save_cols(output, generate_cols(data, int, 'Q113'), 'age')
+    output = save_cols(output, generate_cols(data, str, 'Q114'), 'gender')
+    output = save_cols(output, generate_cols(data, str, 'ERK', 'DBK'), 'erd.exp', 'db.exp')
+    output = save_cols(output, generate_cols(data, str, 'BTK', 'BTK_1', 'BTK_2', 'BTK_3', 'BTK_4', 'BTK_5'), 
+                                                        'BTK.1', 'BTK.2', 'BTK.3', 'BTK.4', 'BTK.5', 'BTK.6')
+    output = save_cols(output, generate_cols(data, str, 'RFK', 'RPK_1', 'RPK_2', 'RPK_3', 'RPK_4', 'RPK_5'),
+                                                        'RFK.1', 'RFK.2', 'RFK.3', 'RFK.4', 'RFK.5', 'RFK.6')
+    output = save_cols(output, generate_cols(data, str, 'Understand1', 'Understand2', 'Use1', 'Use2R', 'Load1', 'ENG1', 'ENG2', 'ENG3'),
+                        'understand.1', 'understand.2', 'use.1', 'use.2', 'load', 'eng.1', 'eng.2', 'eng.3')
 
-    output.to_csv("extracted/questionnaire-answers.csv")
+    output.to_csv('extracted/questionnaire-answers.csv')
 
 data = import_survey(DATA_LOC, N_SUBJ)
 write_output(data)
