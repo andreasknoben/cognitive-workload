9cf99b77b9d04180f1523fc6c8c3716ec7fadaa5
Andreas Knoben <mail@andreasknoben.nl>
2022-05-05 23:14:53 +0200

    Updated gitignore

--- survey_analysis/load_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def import_survey(data_loc, n_subj):
-    '''Loads survey data and extracts last N_SUBJ answers.'''
-
-    survey_data = pd.read_csv(data_loc)
-    subjects_data = survey_data.iloc[-n_subj:]
-    print(f"[INFO] Length of extracted data is {len(subjects_data)}")
-    return subjects_data
